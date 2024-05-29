54631aef0a4b59ace914c465259b07532aaafee2
gmega <giuliano.mega@gmail.com>
2024-05-29 20:29:39 -0300

    fix API bind address, enclose log directive in quotes to avoid leaking ;

--- codex/bin/run.sh
+++ codex/bin/run.sh
@@ -6,16 +6,17 @@ if [ -n "${GATEWAY}" ]; then
     echo "Set ${GATEWAY} as default gateway on eth0"
     ip route change default via ${GATEWAY} dev eth0
 
-    BOOTSTRAP_SPR="--bootstrap-node=$(curl -XGET http://${BOOTSTRAP_NODE}:8080/api/codex/v1/debug/info | jq .spr --raw-output)"
+    BOOTSTRAP_SPR="$(curl -XGET http://${BOOTSTRAP_NODE_ADDR}:8080/api/codex/v1/debug/info | jq .spr --raw-output)"
     echo "Bootstrap node SPR is ${BOOTSTRAP_SPR}"
-    sleep infinity
+    BOOTSTRAP_SWITCH="--bootstrap-node=${BOOTSTRAP_SPR}"
 fi
 
 tcpdump -i eth0 -n -w /opt/codex/logs/dump.pcap &
 
 echo "Starting codex"
 
-/opt/codex/codex ${BOOTSTRAP_SPR}\
+/opt/codex/codex ${BOOTSTRAP_SWITCH}\
     --data-dir=/opt/codex/data \
-    --log-level=INFO;TRACE=autonatservice,relay,relay-client,hpservice,autorelay \
+    "--log-level=INFO;trace:autonatservice,relay,relay-client,hpservice,autorelay" \
+    --api-bindaddr=0.0.0.0 \
     2>&1 | tee /opt/codex/logs/codex.log
